8ab5fee4ae6f6a4608fce0924cd41989f7d6e6de
wilwe <wilwe036@gmail.com>
2025-03-26 16:29:06 +0100

    o chuj tu chodzi czemu raz działa raz nie

--- app/src/main/java/io/github/wilwe21/test/MainActivity.kt
+++ app/src/main/java/io/github/wilwe21/test/MainActivity.kt
@@ -2,47 +2,31 @@ package io.github.wilwe21.test
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.ReportDrawn
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.savedstate.SavedStateRegistry
 
 class MainActivity : ComponentActivity() {
+    val bg = Color(24, 24, 37, 255)
+    val fg = Color(205, 214, 244, 255)
+    val accent = Color(203, 166, 247, 255)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val bg = Color(24, 24, 37, 255)
-        val fg = Color(205, 214, 244, 255)
-        val accent = Color(203, 166, 247, 255)
-        val spacing = 2.dp
-        val borderSize = 3.dp
-        val pad = 7.dp
-        val radii = 7.dp
         setContent {
-            Surface(modifier = Modifier.fillMaxSize(), color = Color(0,0,0,0)) {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = bg
+            ) {
                 Column {
-                    Surface(color = accent) {
-                        Surface(color = bg, modifier = Modifier.padding(borderSize)) {
-                            Column {
-                                var chuj = "Siema Chuju"
-                                Button(onClick = {
-                                    chuj = "Nie Klikaj mnie chuju"
-                                }) {
-                                    Text(text = chuj)
-                                }
-                                Greeting("Cwelu")
-                                Greeting("Cymbale")
-                            }
-                        }
-                    }
+                    Greeting("Chuju", fg)
+                    Tx("Ci", accent)
+                    Tx("w", fg)
+                    Tx("Dupe", accent)
                 }
             }
         }
@@ -50,10 +34,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String, color: Color, modifier: Modifier = Modifier) {
     Text(
-            text = "Siema $name!",
-            modifier = modifier,
-            color = Color(255,255,255)
+        text = "Siema $name!",
+        color = color,
+        modifier = modifier
+    )
+}
+@Composable
+fun Tx(name: String, color: Color, modifier: Modifier = Modifier) {
+    Text(
+        text = "$name!",
+        color = color,
+        modifier = modifier
     )
 }
