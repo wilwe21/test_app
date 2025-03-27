30df3197ccba26faa9eb31f4f80c9cf20de28cb4
wilwe <wilwe036@gmail.com>
2025-03-27 16:10:35 +0100

    o chuj z tym gównem chodzi

--- app/src/main/java/io/github/wilwe21/test/MainActivity.kt
+++ app/src/main/java/io/github/wilwe21/test/MainActivity.kt
@@ -3,30 +3,26 @@ package io.github.wilwe21.test
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.wilwe21.test.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
-    val bg = Color(24, 24, 37, 255)
-    val fg = Color(205, 214, 244, 255)
-    val accent = Color(203, 166, 247, 255)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = bg
-            ) {
-                Column {
-                    Greeting("Chuju", fg)
-                    Tx("Ci", accent)
-                    Tx("w", fg)
-                    Tx("Dupe", accent)
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
                 }
             }
         }
@@ -34,18 +30,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, color: Color, modifier: Modifier = Modifier) {
+fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Siema $name!",
-        color = color,
+        text = "Hello $name!",
         modifier = modifier
     )
 }
+
+@Preview(showBackground = true)
 @Composable
-fun Tx(name: String, color: Color, modifier: Modifier = Modifier) {
-    Text(
-        text = "$name!",
-        color = color,
-        modifier = modifier
-    )
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
 }
